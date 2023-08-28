5c36b2273d765dd934fa90d9f5f42da071188d92
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-08-28 01:15:36 +0100

    add store of state management pinia

--- ctm-front/nuxt.config.ts
+++ ctm-front/nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@element-plus/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@element-plus/nuxt"],
   elementPlus: {},
   $production: {
     routeRules: {
       "/**": { isr: true }
     }
+  },
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      "defineStore", // import { defineStore } from 'pinia'
+      ["defineStore", "definePiniaStore"] // import { defineStore as definePiniaStore } from 'pinia'
+    ]
   },
   $development: {},
   tailwindcss: {},
