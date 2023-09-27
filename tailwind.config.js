d85a489f2b0598e91a4f8fbfc1bbc235545c6f2d
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-09-27 15:07:22 +0100

    reconfigure tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
+const flowbite = require("flowbite/plugin");
 
-delete colors.lightBlue
-delete colors.warmGray
-delete colors.trueGray
-delete colors.coolGray
-delete colors.blueGray
+delete colors.lightBlue;
+delete colors.warmGray;
+delete colors.trueGray;
+delete colors.coolGray;
+delete colors.blueGray;
 
 module.exports = {
-  content: ['./src/**/*.{html,js,vue}'],
+  content: ["./src/**/*.{html,js,vue}"],
   theme: {
     extend: {},
     colors: {
       ...colors,
-      dark: '#1c1c1c',
-      primary: '#18c386',
-      warning: '#e6a23c',
-      success: '#45b8db',
-      danger: '#f56c6c',
-      error: '#f56c6c',
-      info: '#909399',
+      dark: "#1c1c1c",
+      primary: "#18c386",
+      warning: "#e6a23c",
+      success: "#45b8db",
+      danger: "#f56c6c",
+      error: "#f56c6c",
+      info: "#909399"
     },
   },
-  plugins: [],
+  plugins: [flowbite],
   corePlugins: {
     preflight: false,
   },
