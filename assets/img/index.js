1cb8658c8f908c56d8001fb8b0045fe242c0237d
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-09-30 19:21:26 +0100

    add images

---
+++ assets/img/index.js
@@ -0,0 +1,45 @@
+import Bmw from "./brands/Bmw.svg";
+import Ford from "./brands/Ford.svg";
+import Lamborghini from "./brands/Lamborghini.svg";
+import MercedesBenz from "./brands/Mercedes-Benz.svg";
+import Nissan from "./brands/Nissan.svg";
+import Porsche from "./brands/Porsche.svg";
+import Tesla from "./brands/Tesla.svg";
+
+import bmw01 from "./cars/bmw/bmw-1.png";
+import bmw02 from "./cars/bmw/bmw-2.png";
+import bmw03 from "./cars/bmw/bmw-3.png";
+import bmw04 from "./cars/bmw/bmw-4.png";
+import bmw05 from "./cars/bmw/bmw-5.png";
+import bmw06 from "./cars/bmw/bmw-6.png";
+import bmw07 from "./cars/bmw/bmw-7.png";
+import bmw08 from "./cars/bmw/bmw-8.png";
+import bmw09 from "./cars/bmw/bmw-9.png";
+import bmw10 from "./cars/bmw/bmw-10.png";
+import bmw11 from "./cars/bmw/bmw-11.png";
+
+const cars = {
+  bmw01,
+  bmw02,
+  bmw03,
+  bmw04,
+  bmw05,
+  bmw06,
+  bmw07,
+  bmw08,
+  bmw09,
+  bmw10,
+  bmw11
+};
+
+const brandsIcons = {
+  Bmw,
+  Ford,
+  Lamborghini,
+  MercedesBenz,
+  Nissan,
+  Porsche,
+  Tesla
+};
+
+export { brandsIcons, cars };
