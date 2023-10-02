ac49f8ddf774531172f479c173a21b1a8ae073da
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-02 17:45:15 +0100

    create section head component

---
+++ components/global/SectionHead.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="lg:w-3/5 w-5/6 lg:m-0 sm:!m-6 flex flex-col text-black justify-center items-center"
+  >
+    <span
+      class="!font-['Roboto'] front-bold text-gray-500 md:text-xl text-lg text-secondary uppercase tracking-wider text-xl font-medium"
+    >
+      {{ title }}
+    </span>
+    <h1
+      class="text-black font-bold text-center md:text-4xl sm:text-3xl xs:text-2xl text-xl leading-tight"
+    >
+      {{ subtitle }}
+    </h1>
+  </div>
+</template>
+<script setup>
+defineProps({
+  title: String,
+  subtitle: String
+});
+</script>
