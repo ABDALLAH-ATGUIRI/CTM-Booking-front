db186298be76c96d972597d46b0e0ce7477fcb72
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-02 02:17:29 +0100

    create file for constant data

---
+++ constants/index.js
@@ -0,0 +1,32 @@
+import { brandsIcons } from "@/assets/img";
+
+export const brands = [
+  {
+    name: "Bmw",
+    logo: brandsIcons.Bmw
+  },
+  {
+    name: "Mercedes Benz",
+    logo: brandsIcons.MercedesBenz
+  },
+  {
+    name: "Lamborghini",
+    logo: brandsIcons.Lamborghini
+  },
+  {
+    name: "Tesla",
+    logo: brandsIcons.Tesla
+  },
+  {
+    name: "Porsche",
+    logo: brandsIcons.Porsche
+  },
+  {
+    name: "Nissan",
+    logo: brandsIcons.Nissan
+  },
+  {
+    name: "Ford",
+    logo: brandsIcons.Ford
+  }
+];
