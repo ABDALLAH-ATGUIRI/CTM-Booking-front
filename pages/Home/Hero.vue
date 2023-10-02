63d4e14e8b62c7b3dacc3df58121703702004ce7
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-02 02:19:27 +0100

    add responsive hero

---
+++ pages/Home/Hero.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="relative h-full flex lg:flex-row flex-col-reverse justify-end items-start min-h-screen">
+    <div
+      class="absolute w-full -z-40 lg:h-[90%] h-[70%] bg-gradient-to-b from-neutral-900 to-neutral-700"
+    ></div>
+    <div
+      class="lg:absolute -z-10 w-full flex lg:flex-row flex-col justify-center lg:-left-40 -bottom-28"
+    >
+      <img class="w-full" src="~/assets/img/cars/herocar.webp" alt="car" />
+      <div
+        class="lg:w-1/3 flex items-center justify-around [&_.el-progress-circle]:!w-full [&_.el-progress]:!min-w-[100px] [&_.el-progress]:!max-w-[160px] [&_.el-progress]:!w-1/4"
+      >
+        <el-progress
+          v-for="(item, i) in carEngineInfo"
+          :key="i"
+          type="dashboard"
+          :percentage="percentage"
+          :stroke-width="3"
+          color="red"
+        >
+          <template #default>
+            <div class="flex flex-col gap-2 lg:text-white text-[15px]">
+              <span class="text-2xl font-bold italic">{{ item.power }}</span>
+              <span class="">{{ item.unit }}</span>
+              <span class="">{{ item.name }}</span>
+            </div>
+          </template>
+        </el-progress>
+      </div>
+    </div>
+
+    <div
+      class="right-0 w-full h-full xl:w-2/3 p-8 mt-16 flex flex-col items-center text-center whitespace-normal"
+    >
+      <h1 :class="styles.heroHeadText">
+        Fast And Easy Way To Rent <br /><span
+          :class="styles.heroHeadText + ' bg-red-600 px-4'"
+          >A Branded</span
+        >
+        Car
+      </h1>
+      <p :class="`${styles.heroSubText} lg:w-2/3 `">
+        Discover RentalX car rentai options in USA with rent a car Select from a
+        range of car options and local specials.
+      </p>
+
+      <store-buttons></store-buttons>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+
+const percentage = ref(0);
+const carEngineInfo = reactive([
+  {
+    power: 222,
+    unit: "KU/H",
+    name: "TOP SPED"
+  },
+  {
+    power: 92,
+    unit: "c",
+    name: "ENGINE TEMP"
+  },
+  {
+    power: 65,
+    unit: "HP",
+    name: "POWER CAR"
+  }
+]);
+
+onMounted(() => {
+  setInterval(() => {
+    percentage.value = (percentage.value % 100) + 10;
+  }, 500);
+});
+</script>
--- utils/styles.js
+++ utils/styles.js
@@ -4,14 +4,14 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white lg:text-[50px] sm:text-[40px] xs:text-[30px] text-[20px] lg:leading-[80px] mt-2",
+    "!font-['Roboto'] font-bold text-white md:text-6xl sm:text-4xl text-3xl md:!leading-[100px] sm:!leading-[60px] mt-2",
   heroSubText:
-    "text-[#dfd9ff] font-light lg:text-[20px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+    "font-light text-white lg:text-2xl md:text-xl xs:text-lg text-md lg:leading-[40px]",
 
   sectionHeadText:
-    "text-white font-bold md:text-[40px] sm:text-[30px] xs:text-[20px] text-[14px] ",
+    "text-black font-bold text-center md:text-4xl sm:text-3xl xs:text-2xl text-xl leading-tight",
   sectionSubText:
-    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider bg-gradient-to-r from-violet-600 via-fuchsia-600 to-fuchsia-700 bg-clip-text text-transparent text-xl font-medium"
+    "!font-['Roboto'] front-bold text-gray-500 md:text-xl text-lg text-secondary uppercase tracking-wider text-xl font-medium"
 };
 
 export { styles };
